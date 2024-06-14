4efc933f6953ec3fafa0927d74bcadb39ed28150
AndreLuisALCPS <andreluispoisonx67@gmail.com>
2024-06-14 15:07:45 -0300

    edit

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ const db = require('./app/db');
 
 const app = express();
 
-// Configurações de sessão
-app.use(session({
-    secret: 'inceptionsphere_secret',
-    resave: false,
-    saveUninitialized: true,
-}));
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+app.set('views', __dirname + '/app/views')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+
+// Configurações de sessão
+app.use(session({
+    secret: 'inceptionsphere_secret',
+    name: 'sessionId',
+    resave: false,
+    saveUninitialized: false,
+}));
 
 // Rotas
 const indexRouter = require('./app/routes/index');
